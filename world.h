6d6c89324912cd51569fb1e1eb70b61df0c8e1f5
Lukiiy <lukiiy@proton.me>
2026-08-29 21:21:45 -0300

    add WorldEntityCallback pointer & worldScan to world.h

--- world.h
+++ world.h
@@ -1,1 +1,7 @@
 #define WORLD_H
+
+#include <stdint.h>
+
+typedef void (*WorldEntityCallback)(const char *id, int32_t x, int32_t y, int32_t z, void *context);
+
+int worldScan(const char *world_path, int32_t min_chunk_x, int32_t min_chunk_z, int32_t max_chunk_x, int32_t max_chunk_z, WorldEntityCallback callback, void *context);
