d3403662091d47f4a06a904bfbe28919de6b0073
Lukiiy <lukiiy@proton.me>
2026-08-29 21:31:55 -0300

    Add BlockEntityCallback & nbtFindBlockEntities to world.h

--- nbtReader.h
+++ nbtReader.h
@@ -9,3 +9,7 @@ typedef struct {
     size_t size;
     size_t pos;
 } NbtReader;
+
+typedef void (*BlockEntityCallback)(const char *id, int32_t x, int32_t y, int32_t z, void *context);
+
+int nbtFindBlockEntities(const unsigned char *data, size_t size, BlockEntityCallback callback, void *context);
